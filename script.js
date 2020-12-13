86d19201a5a2951cce60588c7ad0b6111762c9df
Altom <Altomhussain2001@yahoo.com>
2020-12-13 20:35:10 +0000

    add 0 digit to the E-No and S-No

--- script.js
+++ script.js
@@ -13,10 +13,16 @@ function makePageForEpisodes(episodeList) {
     let episodeTitle = document.createElement("h3");
     let episodeImage = document.createElement("img");
     let episodeSummary = document.createElement("p");
-
-    episodeTitle.innerText = `${episode.name} - S${episode.season} E${episode.number}`;
+    mainDiv.className = "main-div";
+    episodeTitle.className = "title";
+    episodeImage.className = "image";
+    episodeSummary.className = "summary";
+    
+    episode.season < 10 && episode.number < 10
+      ? (episodeTitle.innerText = `${episode.name} - S0${episode.season} E0${episode.number}`)
+      : (episodeTitle.innerText = `${episode.name} - S0${episode.season} E${episode.number}`);
     episodeImage.src = `${episode.image.medium}`;
-    episodeSummary.innerHTML = `${episode.summary}`
+    episodeSummary.innerHTML = `${episode.summary}`;
     mainDiv.appendChild(episodeTitle);
     mainDiv.appendChild(episodeImage);
     mainDiv.appendChild(episodeSummary);
