25c0730836ebf4116978487d36183fd676c4f893
Altom <Altomhussain2001@yahoo.com>
2020-12-13 19:48:33 +0000

    displayed name, episodeName, sesonNO, EpisondeNO and image

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((episode) => {
+    let mainDiv = document.createElement("div");
+    let episodeTitle = document.createElement("h3");
+    let episodeImage = document.createElement("img");
+    let episodeSummary = document.createElement("p");
+
+    episodeTitle.innerText = `${episode.name} - S${episode.season} E${episode.number}`;
+    episodeImage.src = `${episode.image.medium}`;
+    episodeSummary.innerHTML = `${episode.summary}`
+    mainDiv.appendChild(episodeTitle);
+    mainDiv.appendChild(episodeImage);
+    mainDiv.appendChild(episodeSummary);
+    rootElem.appendChild(mainDiv);
+  });
 }
 
 window.onload = setup;
